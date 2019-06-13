df34abab176d4c329bb8165c2c180d611d5e6714
LB Johnston <lb.johnston@virginaustralia.com>
2019-06-13 21:24:44 +1000

    Make App a arrow function component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.scss';
 import Background from './Background';
 import Timer from './Timer';
 
-function App() {
-  return (
-    <div className="App">
-      <Background>
-        <header className="App-header">Title</header>
-        <main>
-          <Timer minutes={3} seconds={25} />
-        </main>
-      </Background>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Background>
+      <header className="App-header">
+        <input className="App-header-input" value="Title" />
+      </header>
+      <main className="App-main">
+        <Timer minutes={14} seconds={25} />
+      </main>
+    </Background>
+  </div>
+);
 
 export default App;
