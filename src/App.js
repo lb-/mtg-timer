4a1ad363704a2fbd70cdb722562d60f1785f8f10
LB Johnston <lb.johnston@virginaustralia.com>
2019-06-13 20:17:49 +1000

    Add background component and re-org styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.scss';
+import Background from './Background';
 
 function App() {
   return (
     <div className="App">
+      <Background>
+        <header className="App-header">Title</header>
+        <main>
+          <p>
+            <span>25</span>
+            <span>30</span>
+          </p>
+        </main>
+      </Background>
     </div>
   );
 }
---
+++ src/Background.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Background.scss';
+
+const Background = ({ children }) => (
+  <div className="Background">{children}</div>
+);
+
+export default Background;
