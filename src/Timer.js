1975c0bc86fbedb2fd7c51b2f570656983936eef
LB Johnston <lb.johnston@virginaustralia.com>
2019-06-13 21:46:08 +1000

    Set up editable minutes

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Timer.scss';
 
-const Timer = ({ minutes = 0, seconds = 0 }) => (
-  <div className="Timer">
-    <span clasName="Timer-minutes">{minutes}</span>
-    <span className="Timer-spacer">:</span>
-    <span clasName="Timer-seconds">{seconds}</span>
-  </div>
-);
+const Timer = ({ minutes = 0, seconds = 0 }) => {
+  const [min, setMin] = useState(minutes);
+  return (
+    <div className="Timer">
+      <input
+        className="Timer-minutes"
+        onChange={({ target }) => {
+          const num = parseInt(target.value, 10);
+          if (isNaN(num)) {
+            setMin('');
+          } else {
+            setMin(Math.min(Math.max(num, 1), 99));
+          }
+        }}
+        value={min}
+      />
+      <span className="Timer-spacer">:</span>
+      <span className="Timer-seconds">{seconds}</span>
+    </div>
+  );
+};
 
 export default Timer;
