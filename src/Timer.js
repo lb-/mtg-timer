4fa706ccfef697a654866bd88ce6e556dd71843f
LB Johnston <lb.johnston@virginaustralia.com>
2019-06-13 21:54:33 +1000

    move timer on change function to own util

--- src/Timer.js
+++ src/Timer.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import './Timer.scss';
 
+const onChangeMinutes = (setMin, min = 1, max = 99) => ({
+  target: { value },
+}) => {
+  const num = parseInt(value, 10);
+  if (isNaN(num)) {
+    setMin('');
+  } else {
+    setMin(Math.min(Math.max(num, min), max));
+  }
+};
+
 const Timer = ({ minutes = 0, seconds = 0 }) => {
   const [min, setMin] = useState(minutes);
   return (
     <div className="Timer">
       <input
         className="Timer-minutes"
-        onChange={({ target }) => {
-          const num = parseInt(target.value, 10);
-          if (isNaN(num)) {
-            setMin('');
-          } else {
-            setMin(Math.min(Math.max(num, 1), 99));
-          }
-        }}
+        onChange={onChangeMinutes(setMin)}
         value={min}
       />
       <span className="Timer-spacer">:</span>
